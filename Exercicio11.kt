1e84b4816498d521250675133256dc537c055085
Gortin <82157645+gortin1@users.noreply.github.com>
2025-02-12 19:34:52 -0300

    Create Exercicio11.kt

---
+++ Exercicio11.kt
@@ -0,0 +1,9 @@
+fun main(){
+    println("Digite os valores de A, B, C")
+    val a = readln().toInt()
+    val b = readln().toInt()
+    val c = readln().toInt()
+    val delta = (b*b) - 4 * a * c
+
+    println("o delta é de $delta")
+}
