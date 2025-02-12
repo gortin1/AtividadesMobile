ae6cf6d7fd01bdd945a86c8b20f7b4e70dc04772
Gortin <82157645+gortin1@users.noreply.github.com>
2025-02-12 19:30:44 -0300

    Create Exercicio6.kt

---
+++ Exercicio6.kt
@@ -0,0 +1,7 @@
+fun main(){
+    println("Digite um valor abaixo")
+    var a = readln()
+    var ante = a.toInt()+1
+    var depois = a.toInt()-1
+    println("o valor sucessor a $a é $depois e o posterior é $ante")
+}
