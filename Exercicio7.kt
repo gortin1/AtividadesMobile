df717bbd4e684e3bf22f525fd1d6333653f3d2b3
Gortin <82157645+gortin1@users.noreply.github.com>
2025-02-12 19:31:46 -0300

    Create Exercicio7.kt

---
+++ Exercicio7.kt
@@ -0,0 +1,8 @@
+fun main(){
+    println("digite um valor")
+    var a = readln()
+    var dobro = a.toFloat() * 2
+    var terço = a.toFloat()/3
+    println("o dobro do numero $a é $dobro")
+    println("ja a terça parte do numero $a é $terço")
+}
