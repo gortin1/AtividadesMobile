1e1f91dd3e216759dfa34b09ba10994a2b9fc92a
Gortin <82157645+gortin1@users.noreply.github.com>
2025-02-12 19:33:33 -0300

    Create Exercicio9.kt

---
+++ Exercicio9.kt
@@ -0,0 +1,9 @@
+fun exercicio9(){
+    println("digite quantos dolares deseja ver a conversao")
+    var a = readln()
+    var reais = a.toDouble()*5.79
+    var euro = a.toDouble()*0.97
+
+    println("A conversão do valor $a para reais é $reais")
+    println("A conversão do valor $a para euro é $euro")
+}
