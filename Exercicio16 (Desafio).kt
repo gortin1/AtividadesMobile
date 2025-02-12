a6662a846d5d6e85ebb1ff1bc3f2a41bdf836858
Gortin <82157645+gortin1@users.noreply.github.com>
2025-02-12 19:37:42 -0300

    Create Exercicio16 (Desafio).kt

---
+++ Exercicio16 (Desafio).kt
@@ -0,0 +1,10 @@
+fun main(){
+    println("Digite a quantidade de cigarros que você fuma por dia ")
+    val cigarros = readln()
+    println("Digite a quantos anos você fuma ")
+    var tempoFumado = readln()
+    var diasFumado: Int = 365 * tempoFumado.toInt()
+    var tempoPerdido = ((10*(cigarros.toInt() * diasFumado))/60)/24
+
+    println("Voce perdeu num total de $tempoPerdido dias de vida")
+}
