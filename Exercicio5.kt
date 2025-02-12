bc9526b1df3293ac54690d69b2db35169cd9515b
Gortin <82157645+gortin1@users.noreply.github.com>
2025-02-12 19:30:07 -0300

    Create Exercicio5.kt

---
+++ Exercicio5.kt
@@ -0,0 +1,7 @@
+fun main(){
+    println("Digite 2 valores abaixo")
+    var a = readln().toDouble()
+    var b = readln().toDouble()
+    var c = (a + b)/2
+    println("o resultado da media dos dois valores $a e $b é de $c")
+}
